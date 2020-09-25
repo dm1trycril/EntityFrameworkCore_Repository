731054fd837f4fdf1680a7f4a978c5f115d7fa02
lagransh <akrylov008@gmail.com>
2020-09-25 20:28:45 +0300

    Second Commit: Create a database, add a line of connection through the Json file.

---
+++ Ntt_Frmwrk_Cr/Ntt_Frmwrk_Cr/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ntt_Frmwrk_Cr
+{
+    class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppContext(DbContextOptions<AppContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- Ntt_Frmwrk_Cr/Ntt_Frmwrk_Cr/Program.cs
+++ Ntt_Frmwrk_Cr/Ntt_Frmwrk_Cr/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
 
 namespace Ntt_Frmwrk_Cr
 {
@@ -6,7 +10,36 @@ namespace Ntt_Frmwrk_Cr
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            // установка пути к текущему каталогу
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            // получаем конфигурацию из файла appsettings.json
+            builder.AddJsonFile("appsettings.json");
+            // создаем конфигурацию
+            var config = builder.Build();
+            // получаем строку подключения
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppContext>();
+            var options = optionsBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+
+            using (AppContext db = new AppContext(options))
+            {
+                //При первом запуске добаляем два объекта
+                    //User user1 = new User { Name = "Tom", Age = 33 };
+                    //User user2 = new User { Name = "Alice", Age = 27 };
+                    //db.Users.AddRange(user1, user2);
+                    //db.SaveChanges();
+
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            //Console.Read();
         }
     }
 }
