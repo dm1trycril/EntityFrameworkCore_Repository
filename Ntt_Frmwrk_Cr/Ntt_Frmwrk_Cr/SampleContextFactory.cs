00e58c482389bc989ed43416b77d3a8a6eee24be
lagransh <akrylov008@gmail.com>
2020-09-26 16:06:51 +0300

    Added "SampleContextFactory" class. Migration if the context designer accepts the "DbContextOptions" option.

---
+++ Ntt_Frmwrk_Cr/Ntt_Frmwrk_Cr/SampleContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Ntt_Frmwrk_Cr
+{
+    class SampleContextFactory : IDesignTimeDbContextFactory<AppContext>
+    {
+        public AppContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppContext>();
+
+            // получаем конфигурацию из файла appsettings.json
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            IConfigurationRoot config = builder.Build();
+
+            // получаем строку подключения из файла appsettings.json
+            string connectionString = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString, opts => opts.CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds));
+            return new AppContext(optionsBuilder.Options);
+        }
+    }
+}
--- Ntt_Frmwrk_Cr/Ntt_Frmwrk_Cr/User.cs
+++ Ntt_Frmwrk_Cr/Ntt_Frmwrk_Cr/User.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Ntt_Frmwrk_Cr
 {
